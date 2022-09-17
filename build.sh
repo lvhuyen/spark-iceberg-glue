d8966c52c67b2a48ec73f22aef1a841e9feb812b
Huyen <lvhuyen@yahoo.com>
2022-09-17 13:08:49 +1000

    Fix build command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t lvhuyen/spark_iceberg_glue:3.3-0.14-0 --push .
+docker buildx build --platform linux/amd64 -t lvhuyen/spark_iceberg_glue:3.3-0.14-0-slim -f Dockerfile_slim --push .
 docker buildx build --platform linux/amd64,linux/arm64 -t lvhuyen/spark_iceberg_glue:3.3-0.14-0 --push .
 
